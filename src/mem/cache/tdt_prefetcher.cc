86d0737b818d0e2db0b97e1de5b9aefabb6bd819
Markus <markus.servan@gmail.com>
2025-03-23 13:11:23 +0100

    Add Beimnet prefetcher

---
+++ src/mem/cache/tdt_prefetcher.cc
@@ -0,0 +1,178 @@
+#include "mem/cache/prefetch/tdt_prefetcher.hh"
+
+#include "debug/HWPrefetch.hh"
+#include "mem/cache/replacement_policies/base.hh"
+#include "params/TDTPrefetcher.hh"
+
+namespace gem5
+{
+
+GEM5_DEPRECATED_NAMESPACE(Prefetcher, prefetch);
+namespace prefetch
+{
+
+TDTPrefetcher::TDTEntry::TDTEntry(TagExtractor ext)
+    : TaggedEntry()
+{
+    registerTagExtractor(ext);
+    invalidate();
+}
+
+void
+TDTPrefetcher::TDTEntry::invalidate()
+{
+    TaggedEntry::invalidate();
+}
+
+TDTPrefetcher::TDTPrefetcher(const TDTPrefetcherParams &params)
+    : Queued(params),
+      pcTableInfo(params.table_assoc, params.table_entries,
+                  params.table_indexing_policy,
+                  params.table_replacement_policy)
+    {}
+
+TDTPrefetcher::PCTable&
+TDTPrefetcher::findTable(int context)
+{
+    auto it = pcTables.find(context);
+    if (it != pcTables.end())
+        return *(it->second);
+
+    return allocateNewContext(context);
+}
+
+TDTPrefetcher::PCTable&
+TDTPrefetcher::allocateNewContext(int context)
+{
+    assert(context == 0);
+    std::string table_name = name()+".PCTable"+std::to_string(context);
+    pcTables[context].reset(new PCTable(
+        table_name.c_str(),
+        pcTableInfo.numEntries,
+        pcTableInfo.assoc,
+        pcTableInfo.replacementPolicy,
+        pcTableInfo.indexingPolicy,
+        TDTEntry(genTagExtractor(pcTableInfo.indexingPolicy))));
+
+    DPRINTF(HWPrefetch, "Adding context %i with tdt4260 entries\n", context);
+
+    return *(pcTables[context]);
+}
+
+void
+TDTPrefetcher::notifyFill(const CacheAccessProbeArg &arg)
+{
+    //A cache line has been filled in
+}
+
+void
+TDTPrefetcher::BestOffsetPrefetcher::addRecentRequest(Addr addr, Tick time)
+{
+    if(recentRequests.size() >= maxRecentRequests)
+    {
+        recentRequests.pop_front();
+    }
+    recentRequests.push_back({addr, time});
+}
+void
+TDTPrefetcher::BestOffsetPrefetcher::fillOffsetTable()
+{
+    for(int i = 1 ; i < 256; ++i)
+    {
+        bool isPrime = true;
+        for(int j = 1; j <= i; ++j)
+        {
+            int c = i % j;
+            if(c==0)
+            {
+                if(j > 5)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if(isPrime)
+        {
+            offsetTable[i] = {i, 0};
+        }
+    }
+}
+
+
+void
+TDTPrefetcher::BestOffsetPrefetcher::printOffsets() const
+{
+    for (const auto &entry : offsetTable) {
+        DPRINTF(HWPrefetch, "Offset: %d, Score: %d\n", entry.second.offset, entry.second.score);
+    }
+}
+TDTPrefetcher::calculatePrefetch(const PrefetchInfo &pfi,
+                                 std::vector<AddrPriority> &addresses,
+                                    const CacheAccessor &cache)
+{
+    if (!pfi.hasPC()) {
+        DPRINTF(HWPrefetch, "Ignoring request with no PC.\n");
+        return;
+    }
+
+    // access_addr is the memory address (of the cache line) requested
+    Addr access_addr = pfi.getAddr();
+    
+    // access pc is the pc of the inst that requests the cache line
+    Addr access_pc = pfi.getPC();
+
+    // context can be ignored
+    int context = 0;
+    int D = 1;
+
+    // Currently implemented prefetching algorithm: Next line prefetching
+    // TODO: Implement something better!
+    addresses.push_back(AddrPriority(access_addr + blkSize, 0));
+
+    // Can safely be ignored
+    // Get matching storage of entries
+    // Context is 0 due to single-threaded application
+    PCTable& pcTable = findTable(context);
+
+    // Get matching entry for your given PC from the PC Table
+    const TDTEntry::KeyType key{access_pc, false};
+    TDTEntry *entry = pcTable.findEntry(key);
+
+    // Check if you have entry
+    if (entry != nullptr) {
+        // There is an entry for this PC
+        // You might want to update information for this entry
+
+        
+    } else {
+        // No entry for this PC
+        // You might want to make an entry for this PC
+    }
+
+    // The following show you how to add an entry to PCTable for a PC
+    // All slots are by default taken, you must replace a previous slot with new data
+    // Find replacement victim for your new data, update information
+    TDTEntry* victim = pcTable.findVictim(key);
+    victim->lastAddr = access_addr;
+    pcTable.insertEntry(key, victim);
+
+}
+
+uint32_t
+TDTPrefetcherHashedSetAssociative::extractSet(const KeyType &key) const
+{
+    const Addr pc = key.address;
+    const Addr hash1 = pc >> 1;
+    const Addr hash2 = hash1 >> tagShift;
+    return (hash1 ^ hash2) & setMask;
+}
+
+Addr
+TDTPrefetcherHashedSetAssociative::extractTag(const Addr addr) const
+{
+    return addr;
+}
+
+} // namespace prefetch
+} // namespace gem5
---
+++ src/mem/cache/tdt_prefetcher.hh
@@ -0,0 +1,146 @@
+#ifndef __MEM_CACHE_PREFETCH_TDT_PREFETCHER_HH__
+#define __MEM_CACHE_PREFETCH_TDT_PREFETCHER_HH__
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "base/cache/associative_cache.hh"
+#include "base/sat_counter.hh"
+#include "base/types.hh"
+#include "mem/cache/prefetch/queued.hh"
+#include "mem/cache/replacement_policies/replaceable_entry.hh"
+#include "mem/cache/tags/indexing_policies/set_associative.hh"
+#include "mem/cache/tags/tagged_entry.hh"
+#include "mem/packet.hh"
+#include "params/TDTPrefetcherHashedSetAssociative.hh"
+
+namespace gem5
+{
+
+GEM5_DEPRECATED_NAMESPACE(ReplacementPolicy, replacement_policy);
+namespace replacement_policy
+{
+    class Base;
+}
+
+struct TDTPrefetcherParams;
+
+GEM5_DEPRECATED_NAMESPACE(Prefetcher, prefetch);
+namespace prefetch
+{
+
+class TDTPrefetcherHashedSetAssociative : public TaggedSetAssociative
+{
+    protected:
+        uint32_t extractSet(const KeyType &key) const override;
+        Addr extractTag(const Addr addr) const override;
+
+    public:
+        TDTPrefetcherHashedSetAssociative(
+            const TDTPrefetcherHashedSetAssociativeParams &p)
+        : TaggedSetAssociative(p)
+        {}
+
+        ~TDTPrefetcherHashedSetAssociative() = default;
+};
+
+class TDTPrefetcher : public Queued
+{
+
+  protected:
+
+    const struct PCTableInfo
+    {
+        const int assoc;
+        const int numEntries;
+
+        TaggedIndexingPolicy* const indexingPolicy;
+        replacement_policy::Base* const replacementPolicy;
+
+        PCTableInfo(int assoc, int num_entries,
+                    TaggedIndexingPolicy* indexing_policy,
+                    replacement_policy::Base* repl_policy)
+          : assoc(assoc), numEntries(num_entries),
+            indexingPolicy(indexing_policy), replacementPolicy(repl_policy)
+        {
+
+        }
+    } pcTableInfo;
+
+    // A basic entry you can use for designing your prefetcher
+    // You can extend this type of entry with more data like cycle accessed
+    // Recommend to store this in a a map and look through to find matching entries
+    struct TDTEntry : public TaggedEntry
+    {
+        TDTEntry(TagExtractor ext);
+        void invalidate() override;
+
+        
+
+        Addr lastAddr = 0;
+
+
+    };
+
+    // This redefines an associative set as the PC Table, a Set can be indexed
+    // into by a PC, so a PC will go to a single TDTEntry (or return nullptr)
+    using PCTable = AssociativeCache<TDTEntry>;
+
+    // The following can safely be ignored
+    std::unordered_map<int, std::unique_ptr<PCTable>> pcTables;
+
+    PCTable& findTable(int context);
+    PCTable& allocateNewContext(int context);
+    // The preceding can safely be ignored
+
+    void notifyFill(const CacheAccessProbeArg &arg) override;
+
+  public:
+    TDTPrefetcher(const TDTPrefetcherParams &p);
+
+    void calculatePrefetch(const PrefetchInfo &pf1,
+                           std::vector<AddrPriority> &addresses,
+                           const CacheAccessor &cache) override;
+
+};
+
+//Implementation of Best Offset Prefetcher
+//Here we will create a class that will be used to include Recent requests table
+class BestOffsetPrefetcher
+{
+    private:
+        struct RecentRequest
+        {
+            Addr addr;
+            Tick time;
+        };
+
+        struct OffsetScore
+        {
+            int offset;
+            int score;
+        };
+
+        std::vector<RecentRequest> recentRequests;
+        std::unordered_map<int, OffsetScore> offsetTable;
+        int maxRecentRequests; //Size of the recentRequents table
+        int maxOffsets; //Size of the offset Table
+        int maxScore;
+
+        public:
+            BestOffsetPrefetcher(int maxRecentRequests, int maxOffsets)
+             :   maxRecentRequests(maxRecentRequests), maxOffsets(maxOffsets)
+             {
+                fillOffsetTable();
+             }
+             void fillOffsetTable();
+             void addRecentRequest(Addr addr, Tick time);
+             void updateOffsetScore(int offset, int score);
+             int getBestOffset() const;
+             void printOffsets() const; //Method to print offsets
+
+};
+
+
+#endif //_MEM_CACHE_PREFETCH_TDT_PREFETCHER_HH__
